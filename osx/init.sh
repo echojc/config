4c626051c8b0212fcf4482763fe4f7af5fd209f5
Jonathan Chow <jonathan@movio.co>
2015-04-07 14:25:07 +1200

    [osx] Disables ssh gui pasword prompt.

--- osx/init.sh
+++ osx/init.sh
@@ -5,3 +5,4 @@ defaults write com.apple.screencapture type -string "png"
 defaults write .GlobalPreferences com.apple.mouse.scaling -1
 defaults write .GlobalPreferences com.apple.trackpad.scaling -1
 defaults write com.apple.dashboard mcx-disabled -boolean YES
+defaults write org.openbsd.openssh KeychainIntegration -bool false
