6a5f4ddaccf966b44b0ab4c5f39e95104a916f82
Jonathan Chow <me@echo.sh>
2021-03-24 01:02:39 +1300

    [osx] Update KeyRepeat for Big Sur

--- osx/init.sh
+++ osx/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
-defaults write NSGlobalDomain KeyRepeat -int 0
+defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write com.apple.screencapture type -string "png"
 defaults write .GlobalPreferences com.apple.mouse.scaling -1
 defaults write .GlobalPreferences com.apple.trackpad.scaling -1
