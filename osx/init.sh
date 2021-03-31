5f6b022ac666544321cada4e9cf97448d97943c6
Jonathan Chow <me@echo.sh>
2021-04-01 10:37:08 +1300

    [osx] Disabling mouse/trackpad acceleration no longer works.

--- osx/init.sh
+++ osx/init.sh
@@ -3,8 +3,8 @@ defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
 defaults write NSGlobalDomain InitialKeyRepeat -int 10
 defaults write NSGlobalDomain KeyRepeat -int 1
 defaults write com.apple.screencapture type -string "png"
-defaults write .GlobalPreferences com.apple.mouse.scaling -1
-defaults write .GlobalPreferences com.apple.trackpad.scaling -1
+#defaults write -g com.apple.mouse.scaling -int -1
+#defaults write -g com.apple.trackpad.scaling -int -1
 defaults write com.apple.dashboard mcx-disabled -boolean YES
 defaults write org.openbsd.openssh KeychainIntegration -bool false
 
